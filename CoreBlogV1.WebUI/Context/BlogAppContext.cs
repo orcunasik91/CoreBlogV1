0f3b8803136025b970b87a0f241d9e6d23b2357a
Orçun Aşık <asikorcun@gmail.com>
2025-02-09 20:19:52 +0300

    Created BlogAppContext class

---
+++ CoreBlogV1.WebUI/Context/BlogAppContext.cs
@@ -0,0 +1,12 @@
+﻿using CoreBlogV1.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreBlogV1.WebUI.Context;
+public class BlogAppContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CoreBlogV1;User ID=sa;Password=Password1;TrustServerCertificate=True;");
+    }
+    public DbSet<Blog> Blogs { get; set; }
+}
