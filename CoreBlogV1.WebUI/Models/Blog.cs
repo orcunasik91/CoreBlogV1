a1b332b6b4eef65100a5d764095fd8cbee32e126
Orçun Aşık <asikorcun@gmail.com>
2025-02-09 20:19:27 +0300

    Created Blog model

---
+++ CoreBlogV1.WebUI/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreBlogV1.WebUI.Models;
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime PublishDate { get; set; }
+    public string Tags { get; set; }
+    public int LikeCount { get; set; }
+    public int CommentCount { get; set; }
+    public int ViewCount { get; set; }
+    public bool IsStatus { get; set; }
+}
