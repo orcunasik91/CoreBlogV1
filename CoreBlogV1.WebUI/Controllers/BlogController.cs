0a86bd108accebb7e03bafa54deddc4fd0075757
Orçun Aşık <asikorcun@gmail.com>
2025-02-23 18:40:53 +0300

    Added Blog controller

---
+++ CoreBlogV1.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using CoreBlogV1.WebUI.Context;
+using CoreBlogV1.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogV1.WebUI.Controllers;
+public class BlogController(BlogAppContext context) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<Blog> blogs = context.Blogs.Where(blog => blog.IsStatus == true).ToList();
+        return View(blogs);
+    }
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        Blog blog = context.Blogs.Find(id);
+        return View(blog);
+    }
+}
