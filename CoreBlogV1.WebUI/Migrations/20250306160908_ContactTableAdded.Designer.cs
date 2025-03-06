2a615bae54cf4153a0be0ab411aa8cd3fe9b978f
Orçun Aşık <asikorcun@gmail.com>
2025-03-06 19:28:40 +0300

    added migration for contact model

--- CoreBlogV1.WebUI/Migrations/20250306160908_ContactTableAdded.Designer.cs
+++ CoreBlogV1.WebUI/Migrations/20250306160908_ContactTableAdded.Designer.cs
@@ -4,6 +4,7 @@ using CoreBlogV1.WebUI.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoreBlogV1.WebUI.Migrations
 {
     [DbContext(typeof(BlogAppContext))]
-    partial class BlogAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250306160908_ContactTableAdded")]
+    partial class ContactTableAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
