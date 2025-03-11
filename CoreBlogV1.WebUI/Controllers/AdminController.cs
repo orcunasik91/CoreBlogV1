526f97b79f8a1e1ea2ce80833ef8940ee410970a
Orçun Aşık <asikorcun@gmail.com>
2025-03-11 17:04:04 +0300

    Added AdminController and Index page

---
+++ CoreBlogV1.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlogV1.WebUI.Controllers;
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
